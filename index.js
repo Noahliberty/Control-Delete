aac7a0d2cc34389d124fda77679445d3062c5512
Noah <nlcopping@gmail.com>
2021-12-02 00:23:29 +0000

    cleaned up file

--- index.js
+++ index.js
@@ -43,11 +43,8 @@ app.post("/formsubmit", (req, res) => {
             "value": req.body.platform
           }
         ]
-        
-    
     }
-   
-    
+  
     var xhr = new XMLHttpRequest();
     var url = 'https://api.hsforms.com/submissions/v3/integration/submit/25304197/48da8a52-ea69-49fc-b8fd-1ec9435c51f9'
     var final_data = JSON.stringify(data)
@@ -81,50 +78,3 @@ app.post("/formsubmit", (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
     console.log("Listening!")
 });
-
-
-
-// const express       = require("express"),
-//       app           = express(),
-// 	  bodyParser    = require("body-parser"),
-//       mongoose      = require("mongoose"),
-// 	  path 			= require("path");
-
-// let output = "";
-
-
-// // Redirect
-
-// app.get("/", (req, res) => {
-// 	res.redirect("/capi");
-// });
-    
-// // App main page w/ input. End users would copy string in and it would be converted and automatically be displayed // option to copy to clip board or auto copy?
-// app.get("/capi", (req, res) => {
-// 	// let string = req.body.input;
-	
-// 	res.render("capi", {output: output});
-// });
-
-// // Route to handle application logic and rerender the page 
-// app.post("/capi", (req, res) => {
-// 	let userInput = req.body.input;	
-// 	let transform = function siteLinks(str) {
-// // 	Split string - complete
-// 	let array = str.split(" ");
-// // 	Capitalize first char of each item in array after split	
-// let bigLetters = array.map(word => word.replace(word.charAt(0), word.charAt(0).toUpperCase()));
-// // 	Rejoin into new string
-// let finalSentance = bigLetters.join(" ");
-// // 	return result
-// 	res.render("capi", {output: finalSentance});
-// 		}
-
-// 	transform(userInput);
-	
-// });
-
-// app.listen(process.env.PORT || 3000, function() {
-// 	console.log("CAPI App is ready to transform!!!");
-// });
-
